4d2dd269e124e60f16389e97e555e8386be47cea
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2024-05-14 11:25:05 +0200

    Sign up page dropdown style

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -2,19 +2,19 @@
 
 // Import
 import NavigationBar from "../components/NavigationBar";
-import '../styles/signup.css'
-import SignupImage from "../assets/Sign-up imagery.png"
-import React, { useState } from 'react';
-import PrimaryBtn from '../components/buttons/PrimaryBtn'
+import "../styles/signup.css";
+import SignupImage from "../assets/Sign-up imagery.png";
+import React, { useState } from "react";
+import PrimaryBtn from "../components/buttons/PrimaryBtn";
 
 function SignUpPage() {
   const [formData, setFormData] = useState({
-    name: '',
-    description: '',
-    location: '',
-    price: '',
-    bedrooms: '',
-    bathrooms: '',
+    name: "",
+    description: "",
+    location: "",
+    price: "",
+    bedrooms: "",
+    bathrooms: "",
     image: null,
   });
 
@@ -34,12 +34,12 @@ function SignUpPage() {
     console.log(formData);
     // Reset form after submission
     setFormData({
-      name: '',
-      description: '',
-      location: '',
-      price: '',
-      bedrooms: '',
-      bathrooms: '',
+      name: "",
+      description: "",
+      location: "",
+      price: "",
+      bedrooms: "",
+      bathrooms: "",
       image: null,
     });
   };
@@ -54,37 +54,61 @@ function SignUpPage() {
             <img src={SignupImage} alt="blackandwhite" className="signupImage"></img>
           </div>
           <div className="col-4 ">
-
             {/* Sign Up Form */}
-            
+
             <form onSubmit={handleSubmit} className="form1 py-5">
               <h1 className="font-display">Sign Up</h1>
-              <p>Join our community of art enthusiasts and gain access to exclusive exhibitions and events.</p>
+              <p>
+                Join our community of art enthusiasts and gain access to exclusive exhibitions and
+                events.
+              </p>
               <ul>
                 <li>
                   <label>
                     Name and Surname:
-                    <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+                    <input
+                      type="text"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleChange}
+                      required
+                    />
                   </label>
                 </li>
                 <li>
                   <label>
                     Email address:
-                    <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+                    <input
+                      type="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      required
+                    />
                   </label>
                 </li>
                 <li>
                   <label>
                     Mobile Number:
-                    <input type="text" name="mobile" value={formData.mobile} onChange={handleChange} required />
+                    <input
+                      type="text"
+                      name="mobile"
+                      value={formData.mobile}
+                      onChange={handleChange}
+                      required
+                    />
                   </label>
                 </li>
                 <li>
-                  <label>
-                    Choose Your Occupancy:
-                  </label>
-                  <div className="dropdown">
-                    <select className="form-select" name="occupancy" value={formData.occupancy} onChange={handleChange} required>
+                  <label>Choose Your Occupancy:</label>
+                  <div className="dropdown    ">
+                    <select
+                      className="form-select bg-canvas-white-BASE border-2 border-canvas-white-60% rounded-full"
+                      name="occupancy"
+                      value={formData.occupancy}
+                      onChange={handleChange}
+                      required
+                    >
                       <option value="">Select Occupancy</option>
                       <option value="Student">Student</option>
                       <option value="Professional">Professional</option>
@@ -95,22 +119,32 @@ function SignUpPage() {
                 <li>
                   <label>
                     Password:
-                    <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+                    <input
+                      type="password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleChange}
+                      required
+                    />
                   </label>
                 </li>
                 <li>
                   <label>
                     Confirm Password:
-                    <input type="password" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} required />
+                    <input
+                      type="password"
+                      name="confirmPassword"
+                      value={formData.confirmPassword}
+                      onChange={handleChange}
+                      required
+                    />
                   </label>
                 </li>
               </ul>
               <PrimaryBtn label="Sign Up" />
             </form>
-
+          </div>
         </div>
-        </div>
-        
       </div>
     </div>
   );
