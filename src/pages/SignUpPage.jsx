8388e65d9c9a92c1238f81ef86fe0097ff718ff0
KaylaPosthumusOW <231096@virtualwindow.co.za>
2024-05-23 17:10:05 +0200

    Login Functionality

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -5,33 +5,62 @@ import "../styles/signup.css";
 import SignupImage from "../assets/Sign-up imagery.png";
 import PrimaryBtn from "../components/buttons/PrimaryBtn";
 import SecondaryBtn from "../components/buttons/SecondaryBtn";
+import { Link } from "react-router-dom";
 
 function SignUpPage() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [mobile, setMobile] = useState('');
   const [userType, setUserType] = useState('');
+  const [artHouseId, setArtHouseId] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [message, setMessage] = useState('');
 
+  // State for when user selects arthouse
+  // const [selectedArtHouse, setSelectedArtHouse] = useState(false);
+
+
+  // const handleOccupancy = (e) => {
+  //   if(e.target.value === ""){
+
+  //   }
+  // }
+
+  // When input field changes, update the newEvent state (on navbar)
+  // const handleChange = (e) => {
+  //   setNewEvent({
+  //     ...newEvent,
+  //     [e.target.id]: e.target.value,
+  //   });
+  // };
+
+
   const handleSubmit = async (event) => {
+    console.log("Handling Submit")
     event.preventDefault();
     if (password !== confirmPassword) {
       setMessage('Passwords do not match');
       return;
     }
     try {
+      console.log(username,
+        email,
+        mobile,
+        userType,
+        password,
+        artHouseId);
       const response = await axios.post('http://localhost:3001/users/register', {
         username,
         email,
         mobile,
         userType,
         password,
+        artHouseId,
       });
       setMessage('User created successfully!');
     } catch (error) {
-      setMessage(`Error creating user.`);
+      setMessage(`Error creating user: ${error.response ? error.response.data.message : error.message}`);
     }
   };
 
@@ -84,7 +113,7 @@ function SignUpPage() {
                   </label>
                 </li>
                 <li>
-                  <label>Choose Your Occupancy:</label>
+                  <label>Choose Your Occupation:</label>
                   <div className="dropdown">
                     <select
                       className="form-select bg-canvas-white-BASE border-1 border-canvas-white-100% rounded-full"
@@ -94,8 +123,21 @@ function SignUpPage() {
                     >
                       <option value="">Select Occupation</option>
                       <option value="standard">Standard User</option>
-                      <option value="admin">Admin User</option>
-                      <option value="house">Art House Director</option>
+                      <option value="house">Arthouse Director</option>
+                    </select>
+                  </div>
+                  <label className="mt-2">Arthouse:</label>
+                  <div className="dropdown">
+                    <select
+                      className="form-select bg-canvas-white-BASE border-1 border-canvas-white-100% rounded-full"
+                      name="occupancy"
+                      onChange={(e) => setArtHouseId(e.target.value)}
+                      required
+                    >
+                      <option value="a">Select Arthouse</option>
+                      <option value="a">Arthouse 1</option>
+                      <option value="a">Arthouse 2</option>
+                      <option value="a">Arthouse 3</option>
                     </select>
                   </div>
                 </li>
@@ -123,8 +165,8 @@ function SignUpPage() {
                 </li>
               </ul>
               <div>
-                <PrimaryBtn label="Sign Up" />
-                <SecondaryBtn label="Log In " className="m-2" />
+                <PrimaryBtn label="Sign Up" onClick={handleSubmit}/>
+                <Link to="/login"><SecondaryBtn label="Log In " className="m-2" /></Link>
               </div>
             </form>
             {message && <p>{message}</p>}
