bd02b66c13c4f410c281abadb76ab1d97de948ac
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2024-06-01 15:51:23 +0200

    Arthouse name on admin dash

--- src/components/cards/PendingEventCard.jsx
+++ src/components/cards/PendingEventCard.jsx
@@ -1,16 +1,28 @@
 // Admin Dashboard Page
+// Import React hooks
+import { useState, useEffect } from "react";
 
-// Import css
+// Import functions
+import { getHouseById } from "../../services/getExhibitoData";
+
+// Import custom components
 import PrimaryBtn from "../buttons/PrimaryBtn";
 import SecondaryBtn from "../buttons/SecondaryBtn";
 
 function PendingEventCard(props) {
+  const [artHouse, setArtHouse] = useState(null);
+  useEffect(() => {
+    getHouseById(props.artHouseId).then((houseData) => {
+      setArtHouse(houseData);
+    });
+  });
+
   return (
     <div className="card-rectangle lg:flex justify-between items-start">
       <div className="sm:w-full lg:w-[30%]">
         <h4 className="font-body">{props.title}</h4>
         <p className="font-body">{props.desc}</p>
-        <p className="font-body">Arthouse: {props.artHouseId}</p>
+        <p className="font-body">Arthouse: {artHouse && artHouse.name}</p>
       </div>
       <div className="sm:flex lg:block sm:w-full lg:w-fit justify-between">
         <p className="font-body">{`${props.startTime} - ${props.endTime}`}</p>
