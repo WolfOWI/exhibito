6157384cbe0969b59b52cb0702a029010ae74d59
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2024-06-05 11:32:54 +0200

    Event deletion functionality added
    Ability to delete / decline an event on the admin dashboard.

--- src/services/deleteExhibitoData.js
+++ src/services/deleteExhibitoData.js
@@ -3,7 +3,16 @@ import axios from "axios";
 
 // COMMENTS
 // -------------------------------------
+// Delete a comment based on id (delete in admin dash)
 export function deleteCommentById(commentId) {
   return axios.delete(`http://localhost:3001/comments/${commentId}`);
+}
+// -------------------------------------
+
+// EVENTS
+// -------------------------------------
+// Delete an event based on id (decline in admin dash)
+export function deleteEventById(eventId) {
+  return axios.delete(`http://localhost:3001/events/${eventId}`);
 }
 // -------------------------------------
