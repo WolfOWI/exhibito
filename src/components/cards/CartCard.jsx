ecb4b62558ac2eddbb62fedf0099ee3546c1eb9e
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2024-06-06 16:46:01 +0200

    Cart page styling

--- src/components/cards/CartCard.jsx
+++ src/components/cards/CartCard.jsx
@@ -2,36 +2,22 @@
 
 // Import css
 import SecondaryBtn from "../buttons/SecondaryBtn";
-import { Form } from "react-bootstrap";
 
 function CartCard() {
   return (
     <div>
-      <div className="flex items-center my-3">
-        <div className="row">
-          <div className="col-2">
-            <h4 className="font-body mt-1">Event Name</h4>
-          </div>
-          <div className="col-2">
-            <p className="font-body mt-2">Location: Centurion</p>
-          </div>
-          <div className="col-2">
-            <p className="font-body mt-2">Date: 00/00/0000</p>
-          </div>
-          <div className="col-2">
-            <p className="font-body mt-2">Time: 00:00 - 00:00</p>
-          </div>
-          <div className="col-2">
-            <p className="font-body mt-2">Cost: R000.00</p>
-          </div>
-          <div className="col-1"></div>
-          <div className="col-3" style={{ marginLeft: "1040px", marginTop: "-50px" }}>
-            <div className="row">
-              <div className="col-6 btns" style={{ marginLeft: "15px" }}>
-                <SecondaryBtn label="Remove" />
-              </div>
-            </div>
-          </div>
+      <div className="flex justify-between items-center my-3  h-20">
+        <div>
+          <h4 className="font-body">Event Name</h4>
+          <p className="font-body h-2">Location: Centurion</p>
+        </div>
+        <div className="hidden md:block">
+          <p className="font-body h-2">Date: 00/00/0000</p>
+          <p className="font-body h-2">Time: 00:00 - 00:00</p>
+        </div>
+        <div className="flex items-center">
+          <p className="font-body h-2 mr-5">R00.00</p>
+          <SecondaryBtn label="Remove" />
         </div>
       </div>
     </div>
--- src/pages/TicketsPage.jsx
+++ src/pages/TicketsPage.jsx
@@ -5,12 +5,10 @@ import NavigationBar from "../components/NavigationBar";
 import Footer from "../components/Footer";
 import CartCard from "../components/cards/CartCard";
 import "../styles/pendingEventCard.css";
-import SecondaryBtn from "../components/buttons/SecondaryBtn";
 import PrimaryBtn from "../components/buttons/PrimaryBtn";
 
 // Import Bootstrap Components
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 
 function TicketsPage() {
   return (
@@ -18,66 +16,41 @@ function TicketsPage() {
       <NavigationBar />
 
       <Container className="mt-6">
-        {/* Highlighted Events Section */}
+        <div className="flex flex-col">
+          <h1
+            style={{
+              color: "black",
+              fontFamily: "DM Serif Display",
+              fontSize: "39px",
+              float: "left",
+            }}
+          >
+            Added To Your Cart
+          </h1>
 
-        {/* Heading Section */}
-        <div className="Highlighted-Events-Section">
-          <div className="flex flex-col">
-            <h1
-              style={{
-                color: "black",
-                fontFamily: "DM Serif Display",
-                fontSize: "39px",
-                float: "left",
-              }}
-            >
-              Added To Your Cart
-            </h1>
+          <p className="font-body">Below are the events that you have added to your cart.</p>
+        </div>
 
-            <p className="font-body">Below are the events that you have added to your cart.</p>
+        <CartCard />
+        <CartCard />
+        <CartCard />
+        <CartCard />
+        <CartCard />
+
+        {/* Check Out Section Mobile View */}
+        <div className="flex md:hidden flex-col border-t-2 border-ink-silhouette-40% pt-5">
+          <div className="flex justify-between mx-3">
+            <h4 className="font-body fw-bold">Total Cost</h4>
+            <h2 className="font-body">R000.00</h2>
           </div>
-          <CartCard />
-          <CartCard />
-          <CartCard />
-          <CartCard />
-          <CartCard />
-
-          {/* Check Out Section */}
-          <div className="card-rectangle">
-            <div className="row">
-              <div className="col-3">
-                <h4 className="font-body">Proceed to Checkout</h4>
-              </div>
-              <div className="col-8">
-                <p style={{ marginTop: "-11px", fontSize: "19px", marginLeft: "322px" }}>
-                  Quantity:{" "}
-                </p>
-                <p style={{ marginTop: "-20px", marginLeft: "322px" }}>00</p>
-              </div>
-              <div className="col-12">
-                <p
-                  style={{
-                    marginTop: "-11px",
-                    fontSize: "19px",
-                    marginLeft: "860px",
-                    marginTop: "-65px",
-                  }}
-                >
-                  Total Cost:{" "}
-                </p>
-                <p style={{ marginTop: "-20px", marginLeft: "860px", marginTop: "-20px" }}>
-                  R000.00
-                </p>
-              </div>
-              <div className="col-1"></div>
-              <div className="col-3" style={{ marginLeft: "1000px", marginTop: "-61px" }}>
-                <div className="row">
-                  <div className="col-6 btns" style={{ marginLeft: "35px" }}>
-                    <PrimaryBtn label="Check Out" />
-                  </div>
-                </div>
-              </div>
-            </div>
+          <PrimaryBtn label="Check Out" />
+        </div>
+        {/* Check Out Section Desktop View */}
+        <div className="hidden md:flex justify-between border-t-2 border-ink-silhouette-40% pt-5">
+          <h4 className="font-body fw-bold">Total Cost</h4>
+          <div className="flex flex-col items-center">
+            <h4 className="font-body">R00.00</h4>
+            <PrimaryBtn label="Check Out" />
           </div>
         </div>
       </Container>
