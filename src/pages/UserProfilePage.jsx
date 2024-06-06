167f34b2b19230f16f0b4679ed5791bbb52d4d4b
KaylaPosthumusOW <231096@virtualwindow.co.za>
2024-06-06 09:23:27 +0200

    front-end checks

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -62,7 +62,7 @@ function UserProfilePage() {
             </div>
           </div>
           <div className="col-9">
-            <h2 className="font-body mt-3">{user.username}</h2>
+            <h2 className="font-body mt-2">{user.username}</h2>
             <div className="flex items-center">
               <svg
                 className="h-8"
@@ -76,9 +76,18 @@ function UserProfilePage() {
               <p className="font-body mt-3">{user.email}</p>
             </div>
             <div className="flex items-center">
-              <p className="font-body number">{user.mobile}</p>
+              <svg 
+                className="h-8"
+                xmlns="http://www.w3.org/2000/svg" 
+                viewBox="0 -960 960 960" 
+                fill="#D88776"
+              >
+                  <path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12Z"/>
+                </svg>
+                <div className="w-2"></div>
+                <p className="font-body mt-2">{user.mobile}</p>
             </div>
-            <PrimaryBtn label="Log Out" onClick={handleLogout} />
+            <PrimaryBtn label="Log Out" onClick={handleLogout} className="mt-2" />
           </div>
         </div>
       </div>
